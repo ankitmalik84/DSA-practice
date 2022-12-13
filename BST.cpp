72bcf604a1d722639d8ee5643ed594d7dfbc9de3
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2022-12-13 07:12:26 +0530

    delete

--- BST.cpp
+++ BST.cpp
@@ -81,6 +81,59 @@ void takeInput(Node *&root)
         cin >> data;
     }
 }
+Node *deleteFromBST(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    if (root->data == val)
+    {
+        // 0 child
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+
+        // 1 child
+        // left child
+        if (root->left != NULL && root->right == NULL)
+        {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+        // right child
+        if (root->left == NULL && root->right != NULL)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 child
+        if (root->left != NULL && root->right != NULL)
+        {
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+    else if (root->data > val)
+    {
+        // left part me jao
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else
+    {
+        // right partme jao
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
 Node *minVal(Node *root)
 {
     Node *temp;

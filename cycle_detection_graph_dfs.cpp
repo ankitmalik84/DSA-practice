cd8799f2bc76a43046e4abd1c171594557c7eedd
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2022-12-25 13:28:47 +0530

    g

--- cycle_detection_graph_dfs.cpp
+++ cycle_detection_graph_dfs.cpp
@@ -1,38 +1,47 @@
-#include<unordered_map>
-#include<list>
-bool checkCycleDFS(int node,unordered_map<int,bool>&visited,unordered_map<int,bool>&dfsvisited,unordered_map<int,list<int>>&adj){
-    visited[node]=true;
-    dfsvisited[node]=true;
-    
-    for(auto neighbour: adj[node]){
-        if(!visited[neighbour]){
-            bool cycleDetected = checkCycleDFS(neighbour,visited,dfsvisited,adj);
-            if(cycleDetected)
+#include <unordered_map>
+#include <list>
+bool checkCycleDFS(int node, unordered_map<int, bool> &visited, unordered_map<int, bool> &dfsvisited, unordered_map<int, list<int>> &adj)
+{
+    visited[node] = true;
+    dfsvisited[node] = true;
+
+    for (auto neighbour : adj[node])
+    {
+        if (!visited[neighbour])
+        {
+            bool cycleDetected = checkCycleDFS(neighbour, visited, dfsvisited, adj);
+            if (cycleDetected)
                 return true;
         }
-            else if(dfsvisited[neighbour]){
-                return true;
-            }
+        else if (dfsvisited[neighbour])
+        {
+            return true;
+        }
     }
-    dfsvisited[node]=false;
+    dfsvisited[node] = false;
     return false;
 }
-int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
-  // Write your code here.
-    unordered_map<int,list<int>>adj;
-    for(int i=0;i<edges.size();i++){
-        int u=edges[i].first;
+int detectCycleInDirectedGraph(int n, vector<pair<int, int>> &edges)
+{
+    // Write your code here.
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = edges[i].first;
         int v = edges[i].second;
-        
+
         adj[u].push_back(v);
     }
-    unordered_map<int,bool> visited;
-    unordered_map<int,bool> dfsvisited;
-//     call dfs for all components
-    for(int i=1;i<=n;i++){
-        if(!visited[i]){
-            bool cycleFound = checkCycleDFS(i,visited,dfsvisited,adj);
-            if(cycleFound){
+    unordered_map<int, bool> visited;
+    unordered_map<int, bool> dfsvisited;
+    //     call dfs for all components
+    for (int i = 1; i <= n; i++)
+    {
+        if (!visited[i])
+        {
+            bool cycleFound = checkCycleDFS(i, visited, dfsvisited, adj);
+            if (cycleFound)
+            {
                 return true;
             }
         }

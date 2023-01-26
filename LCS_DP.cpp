c8e0353cc628bdee8034bea9d5630223fb5983cc
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-01-26 23:04:44 +0530

    26-jan-23

--- LCS_DP.cpp
+++ LCS_DP.cpp
@@ -17,21 +17,20 @@ int lcs(string s, string t)
 {
     int n = s.size();
     int m = t.size();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+    vector<int> prev(m + 1, 0), cur(m + 1, 0);
     for (int j = 0; j <= m; j++)
-        dp[0][j] = 0;
-    for (int i = 0; i <= n; i++)
-        dp[i][0] = 0;
+        prev[j] = 0;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
         {
             if (s[i - 1] == t[j - 1])
-                dp[i][j] = 1 + dp[i - 1][j - 1];
+                cur[j] = 1 + prev[j - 1];
 
             else
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = max(prev[j], cur[j - 1]);
         }
+        prev = cur;
     }
-    return dp[n][m];
+    return prev[m];
 }

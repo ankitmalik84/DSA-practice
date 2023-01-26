44f9f022e0911fd85276f2f9b386c567ed7c95f2
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-01-26 23:23:51 +0530

    26-jan-23

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Write your code here
+    int p, t;
+    float r;
+    cin >> p >> r >> t;
+    cout << floor((p * r * t) / 100);
+    return 0;
+}

48e76d8d28bdae39c5e296f1c3a8b6e4108e2ec7
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-01-29 06:23:43 +0530

    29-jan-23

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,32 @@
-1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+#include <stdio.h>
+
+int main()
+{
+    int n, j;
+    scanf("%d", &n);
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i > j && arr[i][j] != 0)
+            {
+                printf("0");
+                break;
+            }
+        }
+        if (j != n - 1)
+        {
+            break;
+        }
+    }
+    printf("1");
+    return 0;
+}

05411015aed759aea4edd290ff21d3f069e316b3
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-01-29 06:23:13 +0530

    29-jan-23

--- test.cpp
+++ test.cpp
@@ -1,13 +1,31 @@
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#define N 3
 
-int main()
-{
-    // Write your code here
-    int p, t;
-    float r;
-    cin >> p >> r >> t;
-    cout << floor((p * r * t) / 100);
-    return 0;
+int isUpperTriangular(int mat[N][N]) {
+for (int i = 0; i < N; i++) {
+for (int j = 0; j < N; j++) {
+if (i > j && mat[i][j] != 0) {
+return 0;
+}
+}
+}
+return 1;
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[i])
+        }
+    }
+int mat[N][N] = {{1, 2, 3}, {0, 5, 6}, {0, 0, 9}};
+if (isUpperTriangular(mat)) {
+return 1;
+} else {
+return 0;
+}
+return 0;
 }

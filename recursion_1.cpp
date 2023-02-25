343de4cb050d0812b06a61aa0e8359f6af0196dc
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-02-25 20:51:50 +0530

    25-feb

---
+++ recursion_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void printarr(int arr[], int size, int index)
+{
+    if (index == size)
+    {
+        return;
+    }
+    cout << arr[index] << " ";
+
+    printarr(arr, size, index + 1);
+}
+int main()
+{
+    int arr[] = {1, 3, 4, 5, 6};
+    int size = 5;
+    printarr(arr, size, 0);
+    cout << endl;
+    return 0;
+}
